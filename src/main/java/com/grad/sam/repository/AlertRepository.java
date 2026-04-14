5d2f397589b10f4458debabafdc9aed6bf81cd95
mkozdroj <131193413+mkozdroj@users.noreply.github.com>
2026-04-14 14:39:40 +0200

    Repositories for all tables created (#24)
    Co-authored-by: Marta <martankozdroj@gmail.com>

---
+++ src/main/java/com/grad/sam/repository/AlertRepository.java
@@ -0,0 +1,22 @@
+package com.grad.sam.repository;
+
+import com.grad.sam.model.Alert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AlertRepository extends JpaRepository<Alert, Integer> {
+
+    Optional<Alert> findByAlertRef(String alertRef);
+
+    List<Alert> findByStatus(String status);
+
+    List<Alert> findByAccount_AccountId(Integer accountId);
+
+    List<Alert> findByAssignedTo(String assignedTo);
+
+    List<Alert> findByAlertScoreGreaterThanEqual(Short minScore);
+}
